9b8f3950b4661a7495819695ba7a0526e66de361
YashrajPawar <yashrajpawar33@gmail.com>
2023-12-19 10:42:42 +0530

    added signin and signup routes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 dotenv.config();
 
@@ -17,4 +18,21 @@ mongoose
 
 app.listen(7070, () => {
   console.log("server running on port 7070!!");
+});
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
