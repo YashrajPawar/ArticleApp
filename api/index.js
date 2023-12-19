84c3e826e5c604da367a90873ef2726c3f5bc170
YashrajPawar <yashrajpawar33@gmail.com>
2023-12-19 16:21:22 +0530

    added google auth functionality and routes

--- api/index.js
+++ api/index.js
@@ -23,9 +23,7 @@ app.listen(7070, () => {
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
-app.use("/api/listing", listingRouter);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
